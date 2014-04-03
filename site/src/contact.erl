d730753df2b6fe30bdf9c40335bb20cfb4c83b6f
Aaron Bieber <deftly@gmail.com>
2014-04-03 17:35:34 -0600

    replace about with contact

--- site/src/contact.erl
+++ site/src/contact.erl
@@ -1,13 +1,13 @@
 %% -*- mode: nitrogen -*-
 %% vim: ts=4 sw=4 et
--module (about).
+-module (contact).
 -compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
 
 main() -> #template { file="./site/templates/bare.html" }.
 
-title() -> "CoBUG - About".
+title() -> "CoBUG - Contact".
 
 body() ->
     #container_12 { body=[
@@ -18,7 +18,7 @@ body() ->
 
 inner_body() -> 
     [
-        #h2 {text="About"},
+        #h2 {text="Contact Info"},
         #p {text="The Colorado BSD Users Group"},
         #hr{},
         footer()
