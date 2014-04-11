652d5063318924a7ebf7d7458f2ac5315646fe9a
Aaron Bieber <deftly@gmail.com>
2014-04-11 10:28:29 -0600

    bsd consistency

--- site/src/index.erl
+++ site/src/index.erl
@@ -23,8 +23,8 @@ inner_body() ->
 	#p { text="Created in 2014, CoBUG is a completely open BSD community, \
 	established with the intention of spreading love for the BSDs." },
 	#p { text="CoBSD offers a number of free community oriented features in \
-	the hopes of facilitating growth, awareness, hacking and use of the \
-	*BSDs  in Colorado." },
+	the hopes of facilitating growth, awareness, hacking and use of \
+	*BSD in Colorado." },
 	#hr{},
 	common:footer()
     ].
